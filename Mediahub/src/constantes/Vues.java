46e49b76f236f7b2bf007069f1b2c9c45e9f1fae
Papoune <npapon@live.fr>
2019-11-17 15:03:48 +0100

    Premier commit

---
+++ Mediahub/src/constantes/Vues.java
@@ -0,0 +1,7 @@
+package constantes;
+
+public class Vues {
+
+    public static final String CONNEXION = "/WEB-INF/connexion.jsp";
+
+}
---
+++ Mediahub/src/servlets/Connexion.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import constantes.Vues;
+
+@WebServlet( "/Connexion" )
+public class Connexion extends HttpServlet {
+
+    String                     vue = Vues.CONNEXION;
+    public static final String VUE = "/WEB-INF/connexion.jsp";
+
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request,
+                response );
+    }
+
+    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    }
+
+}
